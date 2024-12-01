53b66b3d4b2ac6bedf00af4f129e3bd8a7eba58b
Nurudeen <abdulmajeednurudeen48@gmail.com>
2024-12-01 16:56:47 +0000

    installed and configured shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { Button } from './components/ui/button';
+
 const App = () => {
   return (
     <div>
       <h1 className="  text-2xl text-blue-700">
         Welcome! School Management System
       </h1>
+      <Button>Test Button</Button>
     </div>
   );
 };
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    target: 'esnext',
+    minify: 'esbuild',
+  },
+  esbuild: {
+    target: 'esnext',
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext',
+    },
+  },
+});
